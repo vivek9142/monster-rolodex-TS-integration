d5090962bb78c5850bf4eaadd740f8e8542ce7a7
Vivek <vivek9142@gmail.com>
2022-06-06 16:44:08 +0530

    made App.tsx compliant with TS by making useState and onSearchChange integrated with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState,ChangeEvent} from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
@@ -6,63 +6,79 @@ import { SearchBox } from './components/search-box/search-box.component';
 import { getData } from './utils/data.utils';
 
 
-//define return type of url returning data
-type Monster = {
+export type Monster = {
   id: string;
   name: string;
   email: string;
 }
 
 const App = () => {
+  /*
+  Now you might be wondering, well, why don't we need to type the use state?
+  So if you hover over it, you should notice that we don't.
+  And the reason for this is because TypeScript is able to infer the type if it has a type
+  already.When we passed in use state.
+  The default initial state that we pass for search field is a empty string.
+  Now an empty string is explicitly typed because there's no way a string can be any
+  other type.
+  
+  So here what you see is that you state is already assigned to this string search field.
+  We'll infer that type and know that it can only be a string.
+  Because if you're giving you say, an initial value of a string, it's only setting strings.
+
+  There's no way that you can set for anything else.
+  So if you try and call set search field and pass it any other type other than string,
+  you will get an error.
+  This also means that we do not need to explicitly type to use state as a result because
+  it can infer that type.
+  */
   const [searchField,setSearchField] = useState('');
-  const [monsters,setMonsters] = useState([]);
+  const [monsters,setMonsters] = useState<Monster[]>([]);
+  /*
+  Set state right here.If you look at you state, notice that right now it's initialized
+  with the never type for the array as the default value.
+  I'm not going to set it to any because that would be extremely not type safe.
+  
+  I'm forcing you to give me a type in order for me to actually set this value.
+  So now what we need to do then is pass it as a generic argument or monsters array.
+  Now what this knows is that U.S. knows we are going to get an initial state of a monsters
+  array.Our set state is going to expect a monsters array as the actual definition of what
+  gets received as the argument.
+  */
+
+
   const [filteredMonsters,setFilteredMonsters] = useState(monsters);
+  /*
+  Similarly, our use state for our filtered monsters takes the monsters from 
+  our actual previous use state as the default initial state.
+  This is explicitly type to monsters array, which we've already done.
 
+  So now this use state is going to also infer that from the initial state.
+
+  There is a lot of inference inside of TypeScript so that we don't have to get too 
+  explicit about everything.
+  */
   useEffect(()=>{
-    // fetch('')
-    //   .then((response) => response.json())
-    //   .then((users) => setMonsters(users));
-
-    /*
-
-    So first we know that we get back the users from us calling our get data.
-    We're calling it data with this URL.
-
-    But here we need to define.
-
-    What the actual type that we're getting is going to be, because right now, 
-    users, we don't actually know, which is why we're saying it's unknown.
-    It's an unknown type.
-
-    We don't know what the type that we're going to get is. We haven't defined it yet.
-    We have to pass it in, as I mentioned, as a generic.
-
-    What we know is that we have, again, those three values we want.
-    So we can say this type is going to be of type monster.
-
-    This monster has an ID, which is a string, has a name, which is a string and 
-    has an email, which is a string.
-
-    And we know this because we were earlier referencing the actual values inside
-    of the website itself
-
-    */
     const fetchUsers = async () => {
-      // we need to reference return data of type Monster array ie. 
-      // <Monster[]> or <Array<Monster>>
-      
+      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users')
+      setMonsters(users);
       /*
-      TSX is able to know, Oh, if I await this, get data, I'm going to definitely get back.
-      A promise with an resolved value of a monster's array away is going to 
-      resolve that value cast as the user.
+      What I want you to see is that once we get our users, let's say I call set monsters 
+      and I pass in our users.
+      Notice how there throw an error argument of a type monsters array is not assignable 
+      to parameter of type sets the action never array type monster array is not assignable
+      to type never array type monster is not assignable to type never what is all this 
+      mean? Especially the never never is a very special keyword inside of TypeScript.
 
-      So of course you're going to get an array of monsters and now users is 
-      explicitly typed to a monster's array, which is what it's expecting.
-
-      And just like that, now we have type safety.
+      It's a very special type.
+      Essentially, the never type is the opposite of the any type.
+      It means that nothing can ever get assigned to never.
+      The reason why it says this is because it needs to make sure that you are defending 
+      your code base and making it strictly tight.
       */
-      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/posts')
     }
+
+    fetchUsers();
   },[]);
 
   useEffect(()=>{
@@ -73,7 +89,7 @@ const App = () => {
     setFilteredMonsters(newFilteredMonsters);
   },[monsters,searchField]);
 
-  const onSearchChange = (e) =>{
+  const onSearchChange = (e:ChangeEvent<HTMLInputElement>):void =>{
     setSearchField(e.target.value.toLocaleLowerCase());
   }
 
